af307af3b076707eb5d4a4a9a83804fbcf451f71
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-04 14:00:40 -0600

    added colors, reset css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,17 @@
 // declare global variables used + word choices available to computer
-var characterChoices = ["Tarzan", "Jane", "Tantor", "Terk", "Kala", "rainforest", "adventure", "gorilla", "leopard", "vines" ];
+var characterChoices = ["tarzan", "jane", "tantor", "terk", "kala", "rainforest", "adventure", "gorilla", "leopard", "vines" ];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var correctGuesses = [];
 var guessesList = [];
+
+
+// function newWord(){
+	// Generate random computer choice and store it in variable computerChoice
+	var computerChoice = characterChoices[Math.floor(Math.random() * characterChoices.length)];
+		// }
+
 //text shown on user side
 var gameText = 
 		"<p> Word to Guess: " + correctGuesses + "</p>"+
@@ -15,56 +22,62 @@ var gameText =
 
 		document.getElementById("game").innerHTML = gameText;
 
+	
+		//reset function called to reset guesses after a win or loss
+		function reset () {
+			guessesLeft = 10;
+			guessesList = [];
+		};
+
+		//variable puzzle holds computer's choice as individual letters
+		var puzzle = computerChoice.split("");
+
+		for (j = 0; j<computerChoice.length; j++) {
+		puzzle[j]="_";
+		correctGuesses.push(puzzle[j]);		
+		}
+
+		var remainingLetters = correctGuesses.length;
+
 		// when key is pressed, trigger game to start reacting
 		document.onkeyup = function(event) {
-
-		// check if user input is alphabetical and if so run the game, if not do not add input to guesses list
-		if (event.keyCode >= 65 && event.keyCode <= 90) {
 
 		// user choice is whatever key was pressed
 		var userChoice = event.key;
 
-		// Generate random computer choice and store it in variable computerChoice
-		var computerChoice = characterChoices[Math.floor(Math.random() * characterChoices.length)];
-		//variable puzzle holds computer's choice as individual letters
-		var puzzle = computerChoice.split("");
-		//puzzle is represented on screen as underscores
-		var puzzle = "_";
-		
+		// check if user input is alphabetical and if so run the game, if not do not add input to guesses list
+		if (event.keyCode >= 65 && event.keyCode <= 90) {
+				
 		// tell correctGuesses array to fill with number of dashes in computerChoice word
 		for (var i = 0; i<computerChoice.length; i++){
-			correctGuesses.push(puzzle);
+				                             
+		if (computerChoice[i] === userChoice) {
+			correctGuesses[i] = userChoice;
+			remainingLetters--;
 
-		}
-
-		if (userChoice === computerChoice.split("")){
-			correctGuesses.push(userChoice);
-			guessesLeft--;
+		} 
+	
+	}	
+		if (guessesLeft !==0) {
 			guessesList.push(userChoice);
-		}
-		//comparing length of computer choice, if userChoice = a letter in computerChoice, change that dash in
-		// correctGuesses to the user input
-		// for (var j = 0; j < computerChoice.length; j++){
-			
-		// 		if (computerChoice[j] === userChoice){
-		// 			correctGuesses[j]===userChoice;
-		// 			guessesLeft--;
-		// 			guessesList.push(userChoice);
-		// 		}
-				//if user did not choose computerChoice letter and guessesLeft is not yet zero
-				//decreases guessesLeft and add userChoice to guessesList
-				// else if (computerChoice[j] !== userChoice && guessesLeft!==0) {
-				// 		guessesLeft--;
-				// 		guessesList.push(userChoice);
+			guessesLeft--;
 
-				// }
-				//if user still didn't guess letter in computer choice and has no guesses left
-				//increase losses by 1
-		// 		if (computerChoice !==userChoice && guessesLeft === 0){
-		// 			losses++;
-		// 		}
-		// };
-		
+		} else { 
+			losses++;
+			reset();
+			
+		}
+
+
+		if (remainingLetters===0){
+			wins++;
+			reset();
+			
+		}
+	
+
+
+			
 		var gameText = 
 		"<p> Word to Guess: " + correctGuesses + "</p>"+
 		"<p> Wins: " + wins + "</p>" +
@@ -73,6 +86,7 @@ var gameText =
 		"<p> Guesses so Far: " + guessesList + "</p>" ;
 
 		document.getElementById("game").innerHTML = gameText;
+
 	//close if/else statement checking alphabetical input
 	}  else {
 		guessesList.pop(userChoice);
