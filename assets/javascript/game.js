bd6b7789a29901b0110a0b0db0bedfcbb6ccb6cd
Cecily Grossmann <grssmnn2@gmail.com>
2018-05-20 12:02:12 -0500

    fixing initial game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // declare global variables used + word choices available to computer
-var characterChoices = ["tarzan", "jane", "tantor", "terk", "kala", "rainforest", "adventure", "gorilla", "leopard", "vines" ];
+var characterChoices = ["tarzan", "jane", "tantor", "terk", "kala", "rainforest", "adventure", "gorilla", "leopard", "vines"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
@@ -13,95 +13,59 @@ var computerChoice = characterChoices[Math.floor(Math.random() * characterChoice
 //variable puzzle holds computer's choice as individual letters
 var puzzle = computerChoice.split("");
 
-//text shown on user side
-var gameText = 
-		"<p> Word to Guess: " + correctGuesses + "</p>"+
-		"<p> Wins: " + wins + "</p>" +
-		"<p> Losses: " + losses + "</p>" +
-		"<p> Guesses Left: " + guessesLeft + "</p>" +
-		"<p> Guesses so Far: " + guessesList + "</p>";
+// use loop to push an underscore for each letter of computerChoice
+for (j = 0; j < computerChoice.length; j++) {
+	puzzle[j] = "_";
+	correctGuesses.push(puzzle[j]);
+}
+// when key is pressed, trigger game to start reacting
+document.onkeyup = function (event) {
 
-		document.getElementById("game").innerHTML = gameText;
+	// user choice is whatever key was pressed
+	var userChoice = event.key;
 
-	
-		//reset function called to reset guesses after a win or loss
-		function reset () {
-			
-			// Generate new random computer choice and store it in variable computerChoice
-			var computerChoice = characterChoices[Math.floor(Math.random() * characterChoices.length)];
-			guessesLeft = 10;
-			guessesList = [];
-	
-		};
+	//reset function called to reset guesses after a win or loss
+	function reset() {
+		// Generate new random computer choice and store it in variable computerChoice
+		computerChoice = characterChoices[Math.floor(Math.random() * characterChoices.length)];
+		guessesLeft = 10;
+		guessesList = [];
 
-	
-		// use loop to push an underscore for each letter of computerChoice
-		for (j = 0; j<computerChoice.length; j++) {
-			puzzle[j]="_";
-			correctGuesses.push(puzzle[j]);		
-		}
-	
+	};
 
-		// when key is pressed, trigger game to start reacting
-		document.onkeyup = function(event) {
-
-
-		// user choice is whatever key was pressed
-		var userChoice = event.key;
-
-		// check if user input is alphabetical and if so run the game, if not do not add input to guesses list
-		if (event.keyCode >= 65 && event.keyCode <= 90) {
-				
+	// check if user input is alphabetical and if so run the game, if not do not add input to guesses list
+	if (event.keyCode >= 65 && event.keyCode <= 90) {
 		// tell correctGuesses array to fill with number of dashes in computerChoice word
-		for (var i = 0; i<computerChoice.length; i++){
-				                             
-		// if userChoice=a letter in the world, print that letter in corresponding space and reduce remaining letter count
-		if (computerChoice[i] === userChoice) {
-			correctGuesses[i] = userChoice;
-			remainingLetters--;
+		for (var i = 0; i < computerChoice.length; i++) {
+			// if userChoice=a letter in the world, print that letter in corresponding space and reduce remaining letter count
+			if (computerChoice[i] === userChoice) {
+				correctGuesses[i] = userChoice;
+				remainingLetters--;
 
-		} 
-	
-	}
+			}
+		}
 		//if guesses left is not zero yet, push user choice to guess list and reduce guesses left by 1 
-		if (guessesLeft !==0) {
+		if(guessesLeft > 0 && correctGuesses.indexOf("_") === -1){
+			wins++;
+			var src = document.getElementById("photos").innerHTML = "<p>" + "<img src='assets/images/tarzanfriends.jpg'/>" + "</p>"
+			reset();
+		}else if (guessesLeft > 0) {
 			guessesList.push(userChoice);
 			guessesLeft--;
 			// if guesses Left is 0, increase losses and reset game
-		} else if (guessesLeft===0) { 
+		} else if (guessesLeft === 0) {
 			losses++;
+			var src = document.getElementById("photos").innerHTML = "<p>" + "Looks like Tarzan isn't remembering anything today!" + "</p>"
 			reset();
-			
-			
-		}else{
-			wins++;
-			reset();
-			
 		}
 
-		
-		var img = document.createElement("img");
-		img.src = "assets/images/tarzanfriends.jpg";
-		var src = document.getElementById("photos");
-		src.appendChild(img);
-    
-      
-
-		// if remainingLetters===0, display image associated with that computerChoice and start audio of Tarzan song else don't show anything
-		// probably something with getElementById.("game").innerHTML = (link to image)
-	
-				
-		var gameText = 
-		"<p> Word to Guess: " + correctGuesses + "</p>"+
-		"<p> Wins: " + wins + "</p>" +
-		"<p> Losses: " + losses + "</p>" +
-		"<p> Guesses Left: " + guessesLeft + "</p>" +
-		"<p> Guesses so Far: " + guessesList + "</p>" ;
+		var gameText =
+			"<p> Word to Guess: " + correctGuesses + "</p>" +
+			"<p> Wins: " + wins + "</p>" +
+			"<p> Losses: " + losses + "</p>" +
+			"<p> Guesses Left: " + guessesLeft + "</p>" +
+			"<p> Guesses so Far: " + guessesList + "</p>";
 
 		document.getElementById("game").innerHTML = gameText;
-
-	//close if/else statement checking alphabetical input
-	}  else {
-		guessesList.pop(userChoice);
 	}
 };
